a1728d253e70628aa2dbf5612529afbdba652c24
psayg <psayg@miumg.edu.gt>
2020-10-25 23:34:47 -0600

    prueba44

---
+++ testCase1.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+/* TestCase1 - Forma Manual
+    Se evalua la existencia de un Administrador
+    por medio de un conteo a la tabla correspondiente de User
+    el resultado esperado debe ser '0' y si se obtiene mayor a eso
+    es que ya existe un Administrador se toma como resultado erroneo.
+*/
+
+use PHPUnit\Framework\TestCase;
+
+final class testCase1 extends TestCase
+{
+    /** @test */
+    public function test_existeAdmin()
+    {
+        $userdata = new userData();
+        $this->assertEquals(
+            0, $userdata->existeAdmin('adminf')
+        );
+    }
+}
+
+?>
